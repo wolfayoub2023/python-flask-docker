524c8a168f000291905874bfde27c56b44dec0b0
wolfayoub2023 <125779865+wolfayoub2023@users.noreply.github.com>
2025-02-22 14:45:52 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -121,5 +121,5 @@ def check_progress(task_id):
     except Exception as e:
         return jsonify({"error": f"Exception occurred: {str(e)}"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
